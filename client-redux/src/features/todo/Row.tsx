09ddfd7651b436f4496c43ed4996d41bf3796262
tlc2880 <tommylcao@gmail.com>
2023-10-02 04:48:02 -0700

    Make additiuonal input validations

--- client-redux/src/features/todo/Row.tsx
+++ client-redux/src/features/todo/Row.tsx
@@ -10,8 +10,8 @@ import {
     TableHead,
     Typography,
     Collapse,
+    IconButton
   } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
 import DeleteIcon from "@mui/icons-material/Delete";
 import DoneIcon from "@mui/icons-material/Done";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
@@ -47,108 +47,108 @@ function createData(
 export default function Row(props: { 
     row: ReturnType<typeof createData>
   }) {
-    const { row } = props;
-    const [open, setOpen] = useState(false);
-    const dispatch = useAppDispatch();
-  
-    const handleDelete = (id: string) => {
-      dispatch(deleteTodo(id));
-    };
-    
-    const completeTodo = (todo: todoType) => {
-      const newTodo = {...todo}
-      newTodo.completed = true;
-      dispatch(updateTodo(newTodo));
-      window.location.reload();
-    }
+  const { row } = props;
+  const [open, setOpen] = useState(false);
+  const dispatch = useAppDispatch();
 
-    return (
-        <>
-          <StyledTableRow key={row.todo_id} sx={{ "& > *": { borderBottom: "unset" } }}>
-            <StyledTableCell component="th" scope="row">
-              {row.todo_id}
-            </StyledTableCell>
-            <StyledTableCell 
-              style={
-                row.completed ? { textDecoration: "line-through", width: 500 } : { width: 500 }
-              }
-              component="th" 
-              scope="row"
-            >
-              {row.description}
-            </StyledTableCell>
-            <StyledTableCell align="left" style={{ width: 300 }}>{row.owner}</StyledTableCell>
-            <StyledTableCell align="left" style={{ width: 150 }}>{row.priority}</StyledTableCell>
-            <StyledTableCell align="right" style={{ width: 100 }}>
-              <IconButton
-                aria-label="expand row"
-                size="small"
-                onClick={() => setOpen(!open)}
-              >
-                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-              </IconButton>
-            </StyledTableCell>
-  
-            <StyledTableCell style={{ width: 100 }} align="right">
-              < EditTodo todo={row} />
-            </StyledTableCell>
-            <StyledTableCell style={{ width: 100 }} align="right">
-            <IconButton
-              component="button"
-              onClick={() => handleDelete(row.todo_id)}
-              color="error"
-            >
-              <DeleteIcon />
-            </IconButton>
-            </StyledTableCell>
-            <StyledTableCell  style={{ width: 100 }} align="right">
-            <IconButton
-              component="button"
-              onClick={() => completeTodo(row)}
-              disabled={row.completed}
-              color="success"
-            >
-              <DoneIcon />
-            </IconButton>
-            </StyledTableCell>
-          </StyledTableRow>
-  
-          <StyledTableRow>
-            <StyledTableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={3}>
-              <Collapse in={open} timeout="auto" unmountOnExit>
-                <Box sx={{ margin: 1 }}>
-                  <Typography variant="h6" gutterBottom component="div">
-                    Time
-                  </Typography>
-                  <Table size="small" aria-label="purchases">
-                    <TableHead>
-                      <StyledTableRow>
-                        <StyledTableCell>Day</StyledTableCell>
-                        <StyledTableCell align="left">Time Range</StyledTableCell>
-                        <StyledTableCell align="right">Duration</StyledTableCell>
-                      </StyledTableRow>
-                    </TableHead>
-                    <TableBody>
-                      <StyledTableRow>
-                        <StyledTableCell component="th" scope="row">
-                          {row.day}
-                        </StyledTableCell>
-                        <StyledTableCell align="left" style={{ width: 300 }}>                  
-                          {row.morning? 'Morning, ': ''} 
-                          {row.afternoon? 'Afternoon, ': ''} 
-                          {row.evening? 'Evening': ''}
-                        </StyledTableCell>
-                        <StyledTableCell align="right" style={{ width: 200 }}>                  
-                          {row.duration} 
-                      </StyledTableCell>
-                      </StyledTableRow>
-                    </TableBody>
-                  </Table>
-                </Box>
-              </Collapse>
-            </StyledTableCell>
-          </StyledTableRow>
-      </>
+  const handleDelete = (id: string) => {
+    dispatch(deleteTodo(id));
+  };
+    
+  const completeTodo = (todo: todoType) => {
+    const newTodo = {...todo}
+    newTodo.completed = true;
+    dispatch(updateTodo(newTodo));
+    window.location.reload();
+  }
+
+  return (
+    <>
+      <StyledTableRow key={row.todo_id} sx={{ "& > *": { borderBottom: "unset" } }}>
+        <StyledTableCell component="th" scope="row">
+          {row.todo_id}
+        </StyledTableCell>
+        <StyledTableCell 
+          style={
+            row.completed ? { textDecoration: "line-through", width: 500 } : { width: 500 }
+          }
+          component="th" 
+          scope="row"
+        >
+          {row.description}
+        </StyledTableCell>
+        <StyledTableCell align="left" style={{ width: 300 }}>{row.owner}</StyledTableCell>
+        <StyledTableCell align="left" style={{ width: 150 }}>{row.priority}</StyledTableCell>
+        <StyledTableCell align="right" style={{ width: 100 }}>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </StyledTableCell>
+
+        <StyledTableCell style={{ width: 100 }} align="right">
+          < EditTodo todo={row} />
+        </StyledTableCell>
+        <StyledTableCell style={{ width: 100 }} align="right">
+        <IconButton
+          component="button"
+          onClick={() => handleDelete(row.todo_id)}
+          color="error"
+        >
+          <DeleteIcon />
+        </IconButton>
+        </StyledTableCell>
+        <StyledTableCell  style={{ width: 100 }} align="right">
+        <IconButton
+          component="button"
+          onClick={() => completeTodo(row)}
+          disabled={row.completed}
+          color="success"
+        >
+          <DoneIcon />
+        </IconButton>
+        </StyledTableCell>
+      </StyledTableRow>
+
+      <StyledTableRow>
+        <StyledTableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={3}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Time
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <StyledTableRow>
+                    <StyledTableCell>Day</StyledTableCell>
+                    <StyledTableCell align="left">Time Range</StyledTableCell>
+                    <StyledTableCell align="right">Duration</StyledTableCell>
+                  </StyledTableRow>
+                </TableHead>
+                <TableBody>
+                  <StyledTableRow>
+                    <StyledTableCell component="th" scope="row">
+                      {row.day}
+                    </StyledTableCell>
+                    <StyledTableCell align="left" style={{ width: 300 }}>                  
+                      {row.morning? 'Morning, ': ''} 
+                      {row.afternoon? 'Afternoon, ': ''} 
+                      {row.evening? 'Evening': ''}
+                    </StyledTableCell>
+                    <StyledTableCell align="right" style={{ width: 200 }}>                  
+                      {row.duration} 
+                  </StyledTableCell>
+                  </StyledTableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </StyledTableCell>
+      </StyledTableRow>
+    </>
     )
   }
   
