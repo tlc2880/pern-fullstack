9f1842a117832d250a7ae1ee8a138567564c122b
tlc2880 <tommylcao@gmail.com>
2023-10-07 05:06:59 -0700

    Client Webpack initial

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Container maxWidth="xl">
-			    <h1 className="input-header" >PERN using MUI, Styled Table, and Stepper Inputs</h1>
+			    <h1 className="input-header" >PERN using MUI, Styled Table, Stepper Inputs, & Webpack</h1>
           <div className="container">
             <InputDialog />
             <InputStepperDialog />
@@ -33,42 +33,3 @@ function App() {
 }
 
 export default App;
-//////////////////////////////////////////////////////////////////////////
-
-// import React, { useState } from "react"
-// import Header from "./components/suspense/Header"
-// import PostsList from "./components/suspense/PostsList"
-///////////////////////////////////////////////////////////////////
-// import { useState, Suspense } from "react";
-// import Header from "./components/suspense/Header";
-// import PostsList from "./components/suspense/PostsList";
-// import SkeletonPost from "./components/suspense/skeletons/SkeletonPost";
-// import { ErrorBoundary } from "react-error-boundary";
-// import ErrorFallback from "./components/suspense/ErrorFallback";
-
-// function App() {
-//   const [currentUserId, setCurrentUserId] = useState(0)
-//   const content = currentUserId === 0
-//     ? <h2 className="message">Select an Employee to view posts</h2>
-//     : (
-//       <ErrorBoundary
-//         FallbackComponent={ErrorFallback}
-//         onReset={() => setCurrentUserId(0)}
-//         resetKeys={[currentUserId]}
-//       >
-//         <Suspense fallback={[...Array(10).keys()].map(i => <SkeletonPost key={i} />)}>
-//           <PostsList currentUserId={currentUserId} />
-//         </Suspense>
-//       </ErrorBoundary>
-//     )
-//   return (
-//     <>
-//       <Header
-//         currentUserId={currentUserId}
-//         setCurrentUserId={setCurrentUserId}
-//       />
-//       {content}
-//     </>
-//   )
-// }
-// export default App;
---
+++ client/webpack/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, '..', './src/main.tsx'),
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, '..', './build'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', './src/index.html'),
+        }),
+    ],
+    devServer: {
+        port: 3000
+    }
+}
